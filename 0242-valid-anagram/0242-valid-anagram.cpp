090014115cc09f6ee5ea2b45d3e8b886907cea93
Vansh Shastri <vanshshastri415@gmail.com>
2026-07-02 11:12:37 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.02%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int hash[26] = {0};
+
+        for(int i = 0 ;i <s.size();i++){
+            hash[s[i] - 'a']++;
+            hash[t[i] - 'a']--;
+        }
+        for(auto it: hash){
+            if(it != 0) return false;
+        }
+        return true;
+    }
+};
